7c48ee811b4aba4b73ecf9cb3337f16705c93302
adong <240732222@qq.com>
2017-04-18 09:36:20 +0800

    weapp

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,12 @@
+Page({
+    onTap: function (event) {
+        // wx.navigateTo({
+        //     url:"../posts/post"
+        // });
+        console.log("hahaha")
+        wx.navigateTo({
+            url: "../movies/movies"
+        });
+        
+    }
+})
